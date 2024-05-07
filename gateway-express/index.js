5b5272403640f63c37b4e1819a4055c87a542729
Denis11333 <denis81619@gmail.com>
2024-05-07 16:57:59 +0300

    update api gateway

--- gateway-express/index.js
+++ gateway-express/index.js
@@ -2,23 +2,44 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const { createProxyMiddleware, fixRequestBody } = require("http-proxy-middleware");
-const https = require( "https" ); 
-const fs = require( "fs" );  
+const {
+  createProxyMiddleware,
+  fixRequestBody,
+} = require("http-proxy-middleware");
+const https = require("https");
+const fs = require("fs");
 const { env } = require("process");
-let dotenv = require('dotenv').config()
+let dotenv = require("dotenv").config();
 
 const app = express();
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-    .replace(/\n/g, '')
-    .split(',')
-    .map((origin) => origin.trim());
+const allowedOrigins = process.env.ALLOWED_ORIGINS.replace(/\n/g, "")
+  .split(",")
+  .map((origin) => origin.trim());
 
-app.use(cors({
-  origin: allowedOrigins.includes('*') ? '*' : allowedOrigins,
-  credentials: true,
-}));
+app.use(
+  cors({
+    origin: allowedOrigins.includes("*") ? "*" : allowedOrigins,
+    credentials: true,
+  })
+);
+
+const checkPassword = (req, res, next) => {
+  const password = req.headers["x-api-password"]; // Перевіряємо заголовок x-api-password
+  const correctPassword = process.env.API_PASSWORD; // Отримуємо пароль з середовища
+  if (password && password === correctPassword) {
+    next(); // Пароль вірний, продовжуємо обробку запиту
+  } else {
+    res.status(403).json({
+      code: 403,
+      status: "Error",
+      message: "Access denied. Invalid or missing API password.",
+      data: new Date(),
+    });
+  }
+};
+
+app.use(checkPassword);
 
 app.use(helmet()); // Add security headers
 app.use(morgan("combined")); // Log HTTP requests
@@ -37,7 +58,7 @@ const services = [
     route: process.env.API_GATEWAY_GOOGLE_MODULES_ROUTE,
     target: `${process.env.API_GATEWAY_GOOGLE_MODULES_PROTOCOL}://${process.env.API_GATEWAY_GOOGLE_MODULES_TARGET}`,
   },
- ];
+];
 
 services.forEach(({ route, target }) => {
   // Proxy options
@@ -66,23 +87,23 @@ app.use((_req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-httpsOptions = null
+httpsOptions = null;
 
-if(process.env.SSL_ENABLED.toLocaleLowerCase() === 'true'){
+if (process.env.SSL_ENABLED.toLocaleLowerCase() === "true") {
   httpsOptions = {
-    key: fs.readFileSync(process.env.SSL_KEY_PATH), 
-    cert: fs.readFileSync(process.env.SSL_CERT_PATH)
-  }
+    key: fs.readFileSync(process.env.SSL_KEY_PATH),
+    cert: fs.readFileSync(process.env.SSL_CERT_PATH),
+  };
 }
 
-if(httpsOptions != null){
+if (httpsOptions != null) {
   const server = https.createServer(httpsOptions, app);
 
   server.listen(PORT, () => {
-    console.log(`Api gateway listening on https with PORT ${PORT}`)
-  })
-}else{
+    console.log(`Api gateway listening on https with PORT ${PORT}`);
+  });
+} else {
   app.listen(PORT, () => {
-    console.log(`Api gateway listening on http with PORT ${PORT}`)
-  })
+    console.log(`Api gateway listening on http with PORT ${PORT}`);
+  });
 }
